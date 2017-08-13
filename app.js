c7a63ae959b301924bd01c49fbbfd5d7efc78fb3
hchen <hchen229@gmail.com>
2017-08-13 02:56:43 -0700

    Update error handlers

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var conf = require("./config");
-var metric = require('metricsclient')(conf)
 var middlewares = require('service-middlewares')(conf)
-var utils = require('servicecommonutils')
-var winston = utils.getWinston(conf.get('env'));
 
 //routes
 var users = require('./routes/users');
@@ -35,41 +32,11 @@ app.use('/facebook', facebook)
 app.use(('/wechat', wechat))
 app.use('/users', middlewares.auth_middleware, users);
 
-//error handler
-//catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('404 Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// development error handler
-if (conf.get("env") === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        return res.json(utils.encodeResponseBody(req, {
-            "message": err.message,
-            "error": err,
-            "errorCode": "INTERNAL_FAILURE"
-        }));
-    });
+app.use(middlewares.error_404_middleware);
+if (conf.get("env") !== 'production') {
+    app.use(middlewares.error_500_middleware_dev);
+} else {
+    app.use(middlewares.error_500_middleware_prod);
 }
-
-// production error handler
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    winston.log('error', err.message, err);
-    metric.errorMetric('IdentityService:Error:' + req.method + ':' + req.url, err, function (error, jsonObj) {
-        if (error != null)
-            return res.json(utils.encodeResponseBody(req, {
-                message: 'Failed to add metric. \n' + err.message,
-                "errorCode": "INTERNAL_FAILURE"
-            }));
-        return res.json(utils.encodeResponseBody(req, {
-            "message": err.message,
-            "errorCode": "INTERNAL_FAILURE"
-        }));
-    })
-});
 
 module.exports = app;
